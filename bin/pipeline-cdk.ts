e9cb42212ad380ed24093ca0a081fd10d388d33d
Eamon Mason <eamon.mason@thomsonreuters.com>
2023-11-23 20:11:21 +0000

    added defaults for env vars for pipeline

--- bin/pipeline-cdk.ts
+++ bin/pipeline-cdk.ts
@@ -5,8 +5,8 @@ import { VPNPipelineStack } from '../lib/vpn-pipeline-stack';
 const app = new cdk.App();
 const pipelineStack = new VPNPipelineStack(app, 'VPNPipelineStack', {
   env: {
-    account: process.env.CDK_DEFAULT_ACCOUNT,
-    region: process.env.CDK_DEFAULT_REGION
+    account: process.env.CDK_DEFAULT_ACCOUNT || '002681522526',
+    region: process.env.CDK_DEFAULT_REGION || 'eu-west-1'
   }
 });
 cdk.Tags.of(pipelineStack).add('application-name', 'wireguard-vpn');
